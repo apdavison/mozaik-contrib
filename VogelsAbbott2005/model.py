dc284c188bc868c4ea024f3c85c6611cf0da5b5d
antolikjan <jan@GPIs-MacBook-Pro.local>
2013-03-25 16:29:12 +0100

    changes to Vogel&Abbot2005 due to new parameter search implementation

--- VogelsAbbott2005/model.py
+++ VogelsAbbott2005/model.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('/home/jan/projects/mozaik/')
 from NeuroTools.parameters import ParameterSet
 from mozaik.models.model import Model
 from mozaik.connectors.fast_connectors import UniformProbabilisticArborization
@@ -26,3 +24,4 @@ class VogelsAbbott(Model):
         UniformProbabilisticArborization(self,'V1L4ExcL4InhConnection',cortex_exc_l4,cortex_inh_l4,self.parameters.l4_cortex_exc.L4ExcL4InhConnection).connect()
         UniformProbabilisticArborization(self,'V1L4InhL4ExcConnection',cortex_inh_l4,cortex_exc_l4,self.parameters.l4_cortex_inh.L4InhL4ExcConnection).connect()
         UniformProbabilisticArborization(self,'V1L4InhL4InhConnection',cortex_inh_l4,cortex_inh_l4,self.parameters.l4_cortex_inh.L4InhL4InhConnection).connect()
+
