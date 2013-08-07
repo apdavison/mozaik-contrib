c30c51c19ac624ccb7a6f1ff9897709571236680
Jan Antolik <antolikjan@gmail.com>
2013-08-07 19:57:16 +0200

    little changes

--- KumarEtAl2005/run_parameter_search.py
+++ KumarEtAl2005/run_parameter_search.py
@@ -5,4 +5,4 @@ import numpy
 #CombinationParameterSearch(SlurmSequentialBackend(num_threads=4,num_mpi=1),{'l4_cortex_exc.L4ExcL4ExcConnection.weights' : [0.002,0.004],'l4_cortex_inh.L4InhL4ExcConnection.weights' : [0.2,0.51]}).run_parameter_search()
 CombinationParameterSearch(SlurmSequentialBackend(num_threads=1,num_mpi=64),{'l4_cortex_exc.L4ExcL4ExcConnection.weights' : numpy.linspace(0.003,0.006,2),'l4_cortex_inh.L4InhL4ExcConnection.weights' : numpy.linspace(0.048,0.096,2)}).run_parameter_search()
 
-#CombinationParameterSearch(SlurmSequentialBackend(num_threads=1,num_mpi=1),{''l4_cortex_exc.params.artificial_stimulators.background_act.params.exc_firing_rate' : numpy.linspace(0.0,100000,400)}).run_parameter_search()
+#CombinationParameterSearch(SlurmSequentialBackend(num_threads=1,num_mpi=1),{'l4_cortex_exc.params.artificial_stimulators.background_act.params.exc_firing_rate' : numpy.linspace(0.0,100000,400)}).run_parameter_search()
