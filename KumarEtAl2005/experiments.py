57a2c30a239638e5546164788afdbb0e7e09e74a
Jan Antolik <antolikjan@gmail.com>
2013-08-07 19:51:13 +0200

    little changes

--- KumarEtAl2005/experiments.py
+++ KumarEtAl2005/experiments.py
@@ -7,7 +7,7 @@ def create_experiments(model):
     
     return  [
                            #Lets kick the network up into activation
-                           PoissonNetworkKick(model,duration=10*8*7,drive_period=200.0,sheet_list=["V1_Exc_L4","V1_Inh_L4"],recording_configuration={'component' : 'mozaik.sheets.population_selector.RCRandomPercentage','params' : {'percentage' : 100.0}},lambda_list=[3000.0,3000.0],weight_list=[0.006,0.006]),
+                           PoissonNetworkKick(model,duration=10*8*7,drive_period=200.0,sheet_list=["V1_Exc_L4","V1_Inh_L4"],recording_configuration={'component' : 'mozaik.sheets.population_selector.RCRandomPercentage','params' : {'percentage' : 100.0}},lambda_list=[5000.0,5000.0],weight_list=[0.006,0.006]),
                            #Spontaneous Activity 
                            NoStimulation(model,duration=3*8*7),
             ]
