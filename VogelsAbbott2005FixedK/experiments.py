75f35b9d59dc2fb231c935f4d799491ce967571e
antolikjan <antolikjan@ClusterU.debian.cluster>
2013-08-16 16:12:01 +0200

    added VogelsAbbott2005FixedK

---
+++ VogelsAbbott2005FixedK/experiments.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/ipython -i 
+from mozaik.experiments import *
+from mozaik.sheets.population_selector import RCRandomPercentage
+from parameters import ParameterSet
+    
+def create_experiments(model):
+    
+    return  [
+                           #Lets kick the network up into activation
+                           PoissonNetworkKick(model,duration=10*5*8*7,drive_period=200.0,sheet_list=["V1_Exc_L4","V1_Inh_L4"],recording_configuration={'component' : 'mozaik.sheets.population_selector.RCRandomPercentage','params' : {'percentage' : 100.0}},lambda_list=[400.0,400.0],weight_list=[0.004,0.004]),
+                           #Spontaneous Activity 
+                           NoStimulation(model,duration=3*8*7),
+            ]
