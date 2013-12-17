11289d2d8a22ceb6578a22c339e74912cca22177
Jan Antolik <antolikjan@gmail.com>
2013-12-17 17:19:05 +0100

    changes recording_configurations to stimulation_configuration in all experiments.py files

--- SelfSustainedPushPull/experiments.py
+++ SelfSustainedPushPull/experiments.py
@@ -8,7 +8,7 @@ def create_experiments(model):
     
     return  [
                            #Lets kick the network up into activation
-                           PoissonNetworkKick(model,duration=8*8*7,drive_period=200.0,sheet_list=["V1_Exc_L4","V1_Inh_L4"],recording_configuration={'component' : 'mozaik.sheets.population_selector.RCRandomPercentage','params' : {'percentage' : 100.0}},lambda_list=[400.0,400.0],weight_list=[0.0006,0.0006]),
+                           PoissonNetworkKick(model,duration=8*8*7,drive_period=200.0,sheet_list=["V1_Exc_L4","V1_Inh_L4"],stimulation_configuration={'component' : 'mozaik.sheets.population_selector.RCRandomPercentage','params' : {'percentage' : 100.0}},lambda_list=[400.0,400.0],weight_list=[0.0006,0.0006]),
                            #Spontaneous Activity 
                            NoStimulation(model,duration=2*5*3*8*7),
                            # Measure orientation tuning with full-filed sinusoidal gratins
