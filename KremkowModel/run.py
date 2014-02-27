d7d6bcef4a6e857aaa066e94d819210922c8f918
Jan Antolik <antolikjan@gmail.com>
2014-02-27 16:01:48 +0100

    created new composite plots

--- KremkowModel/run.py
+++ KremkowModel/run.py
@@ -26,7 +26,7 @@ MPI_ROOT = 0
 
 logger = mozaik.getMozaikLogger()
 
-if True:
+if False:
     data_store,model = run_workflow('FFI',PushPullCCModel,create_experiments)
     #model.connectors['V1L4ExcL4ExcConnection'].store_connections(data_store)    
     #model.connectors['V1L4ExcL4InhConnection'].store_connections(data_store)    
@@ -37,7 +37,7 @@ if True:
     
 else: 
     setup_logging()
-    data_store = PickledDataStore(load=True,parameters=ParameterSet({'root_directory':'FFI_BIG_____', 'store_stimuli' : False}),replace=True)
+    data_store = PickledDataStore(load=True,parameters=ParameterSet({'root_directory':'FFI_OLD_____', 'store_stimuli' : False}),replace=True)
     logger.info('Loaded data store')
 
 if mpi_comm.rank == MPI_ROOT:
