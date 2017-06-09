5d7a300ca2b5bbbf530e1df8d26dd9f74f0f5ad1
Jan Antolik <antolikjan@gmail.com>
2017-06-09 10:58:27 +0200

    changes

--- MorganTaylorModel/parameter_search_analysis.py
+++ MorganTaylorModel/parameter_search_analysis.py
@@ -1,6 +1,6 @@
 import sys
 from mozaik.meta_workflow.parameter_search import parameter_search_run_script_distributed_slurm
-assert len(sys.argv) == 2
+assert len(sys.argv) == 2, "Wrong number of arguments, required 2 supplied %g" % (len(sys.argv))
 directory = sys.argv[1]
 
-parameter_search_run_script_distributed_slurm("MorganTaylorModel",directory,'run_analysis.py',32)
+parameter_search_run_script_distributed_slurm("MorganTaylorModel",directory,'run_analysis.py',4)
